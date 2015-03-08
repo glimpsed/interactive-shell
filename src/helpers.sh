04c8b51b3650bf9b412179ec95945df584c54f62
drKraken <kuchuklehjs@gmail.com>
2015-03-08 13:29:53 +0200

    make sexy helpers

--- src/helpers.sh
+++ src/helpers.sh
@@ -1,29 +1,20 @@
 
 # print success helper
-# @arg [string] text
-# @return 0
-
-function success_echo {
+success_echo() {
 	local text=$1
 	echo -e "\033[0;32m$text\033[0m"
 	return 0
 }
 
 # print error message helper
-# @arg [string] text
-# @return 0
-
-function error_echo {
+error_echo() {
 	local text=$1
 	echo -e "\033[0;31m$text\033[0m"
 	return 0
 }
 
 # print term message helper
-# @arg [string] text
-# @return 0
-
-function term_echo {
+term_echo() {
 	local text=$1
 	echo -e "\033[1;33m$text\033[0m"
 	return 0
